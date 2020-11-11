683fc8bb5eee4b775665508f6fb5505d9de63322
cooljasonmelton <cooljasonmelton>
2020-11-11 10:37:32 -0600

    set up create to dos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,7 @@
 class TodosController < ApplicationController
+    def create
+        todo = Todo.create(name: params[:name], text: params[:text], importance: params[:importance], topic_id: params[:topicId])
+        render json: Topic.all, include: :todos
+    end
 end
+
