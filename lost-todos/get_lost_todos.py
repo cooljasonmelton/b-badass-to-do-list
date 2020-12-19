5b0fb802cce464a111e0cee284f85e82fff569a2
cooljasonmelton <cooljasonmelton>
2020-12-19 00:50:49 -0600

    py script for getting todos from log

--- lost-todos/get_lost_todos.py
+++ lost-todos/get_lost_todos.py
@@ -1,3 +1,18 @@
 # write script that scrapes todos from old rails logs in 'lost_todos.txt'
 
 
+f = open('lost_todos.txt', 'r')
+content = f.read()
+for line in content.split('\n'):
+    if 'Create' in line and 'todos' in line:
+        todo_from_name = line.split('[["name", "')[1]
+        todo_name_text = todo_from_name.split(', ["importance",')[0]
+        split_name_text = todo_name_text.split('"], ["')
+        print(split_name_text[0])
+        print(split_name_text[1])
+
+        
+
+
+
+f.close()
